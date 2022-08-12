d594a70d6625909b416e1c6b81eace02e5192b1f
Marsadiwu <Marsadiwu@users.noreply.github.com>
2022-08-12 10:57:18 +0200

    forecast

--- src/app.js
+++ src/app.js
@@ -48,10 +48,12 @@ function showCurrentData(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "c88b70400da19a39ad34ae161f43c93f";
+  let apiKey = "9bfa0003d71528a0ff8320e51ac65a12";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showCurrentData);
 }
@@ -88,5 +90,49 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+function getForecast(coordinates) {
+  let apiKey = "9bfa0003d71528a0ff8320e51ac65a12";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div id="forecast-day">${formatWeekday(forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" id="forecast-icon" />
+                <div class="forecast-temp">
+                  <span id="forecast-max">${Math.round(
+                    forecastDay.temp.max
+                  )}</span> °|
+                  <span id="forecast-min">${Math.round(
+                    forecastDay.temp.min
+                  )} </span> °
+                </div>
+              </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function formatWeekday(currentMilliseconds) {
+  let date = new Date(currentMilliseconds * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return days[day] + 1;
+}
 
 search("Berlin");
